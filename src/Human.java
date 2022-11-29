16685e4fc5332d349ea7e1309f14c28154ac9115
main <you@example.com>
2022-11-29 11:16:10 +0300

    first commit

---
+++ src/Human.java
@@ -0,0 +1,35 @@
+public class Human {
+    int age;
+    private double height;
+    private double weight;
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Human(int age, double height, double weight) {
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        int count = 0;
+//        int a,b = 1, c = 1;
+//        double r = 1.4;
+//        float f = 1.4f;
+//        boolean trueFalse = true;
+//        String str = "Stroka";
+//        char cc = 'r'; char ccc = 143;
+//
+//        if (true) {
+//            System.out.println("Fuck yu");
+//        } else if (2==0) {
+//
+//        } else {
+//
+//        }
+//
+////        for (int i = 0; i < 10; i++) {
+////            System.out.print(i + " ");
+////        }
+//
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+        Human humanDima = new Human(18, 1.7,60.2);
+        Human humanOlga = new Human(19,1.8, 60.2);
+
+        System.out.println("Dima " + humanDima.age);
+        System.out.println("Olga " + humanOlga.age);
+
+    }
+}
